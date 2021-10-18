eeae567775d769c39720cc2c4372b94834a3c7e8
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2021-10-18 16:00:10 +0300

    Moved to Draft JS
    Adding toolbar

--- src/EditorContainer.tsx
+++ src/EditorContainer.tsx
@@ -1,69 +1,32 @@
-import React, { useRef, KeyboardEvent, useState, createRef, RefObject, useEffect } from 'react'
-import EditableBlock, { BlockType } from './EditableBlock';
-import { addBlockToList, createNewBlock, getSelectedSpanRef, getUid, removeBlockFromList, setCaret } from './utils';
+import { DraftEditorCommand, DraftHandleValue, Editor, EditorState, RichUtils } from 'draft-js'
+import React, { useState } from 'react'
 
-const initialBlockList:BlockType[] = [
-  {
-    id: getUid(),
-    tag: "p",
-    textContent: [["normal",""]],
-    spanRef: createRef<HTMLSpanElement>()
-  }
-]
+import 'draft-js/dist/Draft.css'
 
 export default function EditorContainer() {
-  const [blockList, setBlockList] = useState(initialBlockList);
-  const [current, setCurrent] = useState(0);
-  const div = useRef<HTMLDivElement>(null);
-  const span = useRef<HTMLSpanElement>(null);
+  const [editorState, seteditorState] = useState(() => EditorState.createEmpty())
 
-  const updateEditor = (updateParams:[BlockType[],number]) => {
-    const [nexBlockList, nextCurrent] = updateParams;
-    setBlockList(nexBlockList);
-    setCurrent(nextCurrent);
-  }
+  const handleKeyCommand = (command:DraftEditorCommand, editorState:EditorState):DraftHandleValue => {
+    const newState = RichUtils.handleKeyCommand(editorState, command)
 
-  const onKeyDownHandler = (event:KeyboardEvent) => {
-    switch (event.key) {
-      case "Backspace":
-        const currentSpan = getSelectedSpanRef();
-        if (currentSpan && currentSpan.textContent?.length === 1) {
-          event.preventDefault();
-          if (blockList.length > 1) {
-            updateEditor(removeBlockFromList(blockList, currentSpan));
-          }
-        }
-        break;
-      case "Enter":
-        event.preventDefault();
-        updateEditor(addBlockToList(blockList,createNewBlock()));
-      default:
-        break;
+    if (newState) {
+      seteditorState(newState)
+      return 'handled'
     }
+
+    return 'not-handled'
   }
 
-  useEffect(() => {
-    const currentSpan = blockList[current].spanRef.current;
-    currentSpan && setCaret(currentSpan);
-    console.log(currentSpan);
-  }, [current]);
+  const toggleBold = () => seteditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'))
 
   return (
-    <div
-    className="block"
-    ref={div}
-    contentEditable={true}
-    onKeyDown={onKeyDownHandler}
-    suppressContentEditableWarning={true}
-    role="textbox"
-    spellCheck={true}
-    >
-      {
-        blockList.map(block => <EditableBlock
-        key={block.id}
-        spanRef={block.spanRef}
-        />)
-      }
+    <div>
+      <button onClick={toggleBold}>Bold</button>
+      <Editor
+        editorState={editorState}
+        onChange={seteditorState}
+        handleKeyCommand={handleKeyCommand}
+      />
     </div>
   )
 }
---
+++ src/EditorToolbar.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const blocType = [
+  {
+    label: "Title",
+    style: "header-one"
+  },
+  {
+    label: "Paragraph",
+    style: "paragraph"
+  },
+  {
+    label: "Quote",
+    style: "blockquote"
+  },
+  {
+    label: "Code",
+    style: "code-block"
+  },
+  {
+    label: "Ordered List",
+    style: "ordered-list-item"
+  },
+  {
+    label: "Unordered List",
+    style: "unordered-list-item"
+  }
+]
+
+const textFormat = [
+  {
+    label: "Bold",
+    style: "BOLD"
+  },
+  {
+    label: "Italic",
+    style: "ITALIC"
+  },
+  {
+    label: "Underline",
+    style: "UNDERLINE"
+  },
+  {
+    label: "Align Left",
+    style: "left"
+  },
+  {
+    label: "Align Right",
+    style: "right"
+  },
+  {
+    label: "Align Center",
+    style: "right"
+  }
+]
+
+export default function EditorToolbar() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
